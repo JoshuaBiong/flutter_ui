7c9cd814f2e2e75782971ec483f7cebe7b913de1
Joshua Biong <joshuabiong7@gmail.com>
2024-03-22 16:27:24 +0800

    separate the searchbar widget to the widget folder for cleaner code

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_01/widgets/search_bar.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class Homepage extends StatelessWidget {
@@ -8,55 +9,8 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: appBar(),
-        body: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                      color: const Color(0xff1D1617).withOpacity(0.11),
-                      blurRadius: 40,
-                      spreadRadius: 0.0),
-                ],
-              ),
-              margin: const EdgeInsets.only(top: 40.0, left: 20.0, right: 20.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Search',
-                  fillColor: Colors.white,
-                  filled: true,
-                  contentPadding: const EdgeInsets.all(15),
-                  prefixIcon: Padding(
-                    padding: const EdgeInsets.all(15),
-                    child: SvgPicture.asset('./assets/icon/Search.svg'),
-                  ),
-                  suffixIcon: SizedBox(
-                    width: 90.0,
-                    child: IntrinsicHeight(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          const VerticalDivider(
-                            endIndent: 12.0,
-                            indent: 12.0,
-                            thickness: 1.0,
-                            color: Colors.black38,
-                          ),
-                          Padding(
-                              padding: const EdgeInsets.all(15),
-                              child:
-                                  SvgPicture.asset('./assets/icon/Filter.svg')),
-                        ],
-                      ),
-                    ),
-                  ),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                      borderSide: BorderSide.none),
-                ),
-              ),
-            )
-          ],
+        body: const Column(
+          children: [MySearchBar()],
         ));
   }
 
