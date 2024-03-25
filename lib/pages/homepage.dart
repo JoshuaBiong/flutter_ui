9bae7edbc23b84cc2d210f260926f1e047b1a2b9
Joshua Biong <joshuabiong7@gmail.com>
2024-03-25 08:05:30 +0800

    perform the illimination debugging to indentify the issue of the white screen when the listView. builder is included

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter/widgets.dart';
 import 'package:flutter_01/model/category_model.dart';
 import 'package:flutter_01/widgets/search_bar.dart';
+// import 'package:flutter_01/widgets/search_bar.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class Homepage extends StatefulWidget {
@@ -21,67 +24,92 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     _getCategories();
     return Scaffold(
-        appBar: appBar(),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const MySearchBar(),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 30.0),
-                const Padding(
-                  padding: EdgeInsets.only(left: 20),
-                  child: Text(
-                    "Catgorey",
-                    style:
-                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.w800),
-                  ),
-                ),
-                const SizedBox(height: 15.0),
-                Container(
-                  height: 150.0,
-                  color: Colors.green,
-                  child: ListView.builder(itemBuilder: (context, index) {
-                    return Container();
-                  }),
-                )
-              ],
-            )
-          ],
-        ));
-  }
-
-// ------------------------------   AppBar WIDGET
-  AppBar appBar() {
-    return AppBar(
-      centerTitle: true,
-      title: const Text(
-        "Breakfast",
-        style: TextStyle(fontWeight: FontWeight.bold),
-      ),
-      leading: Container(
-        alignment: Alignment.center,
-        margin: const EdgeInsets.all(10.0),
-        decoration: BoxDecoration(
-            color: Colors.black12, borderRadius: BorderRadius.circular(8.0)),
-        child: SvgPicture.asset(
-          './assets/icon/arrowleft.svg',
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "Breakfast",
+          style: TextStyle(fontWeight: FontWeight.bold),
         ),
-      ),
-      actions: [
-        Container(
-          margin: const EdgeInsets.all(10.0),
-          width: 37.0,
-          height: 37.0,
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            color: Colors.black12,
-            borderRadius: BorderRadius.circular(8.0),
+        leading: GestureDetector(
+          onTap: () {
+            if (kDebugMode) {
+              print('being tapped');
+            }
+          },
+          child: Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+                color: Colors.black12,
+                borderRadius: BorderRadius.circular(8.0)),
+            child: SvgPicture.asset(
+              './assets/icon/arrowleft.svg',
+            ),
           ),
-          child: SvgPicture.asset('./assets/icon/dots.svg'),
-        )
-      ],
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {
+              if (kDebugMode) {
+                print('being tapped in action');
+              }
+            },
+            child: Container(
+              margin: const EdgeInsets.all(10.0),
+              width: 37.0,
+              height: 37.0,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.black12,
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: SvgPicture.asset('./assets/icon/dots.svg'),
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        // crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const MySearchBar(),
+          Column(
+            // crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 30.0),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 20),
+                    child: Text(
+                      "Catgorey",
+                      style: TextStyle(
+                          fontSize: 20.0, fontWeight: FontWeight.w800),
+                    ),
+                  ),
+                  const SizedBox(height: 15.0),
+                  Container(
+                    height: 150.0,
+                    color: Colors.green,
+                    child: ListView.builder(itemBuilder: (context, index) {
+                      return Container();
+                    }),
+                  )
+                  // Container(
+                  //   height: 150.0,
+                  //   color: Colors.green,
+                  //   child: ListView.builder(
+                  //     itemBuilder: (context, index) {
+                  //       return Container();
+                  //     },
+                  //   ),
+                  // )
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
