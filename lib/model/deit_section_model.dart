3c514c8e62e7bfd3118b197cfeff1d739b6ede71
Joshua Biong <joshuabiong7@gmail.com>
2024-03-26 11:38:39 +0800

    Diet Section

--- lib/model/deit_section_model.dart
+++ lib/model/deit_section_model.dart
@@ -1,18 +1,58 @@
 class DietSectionModel {
   String name;
   String iconPath;
-  int amount;
+  String level;
+  String duration;
+  String calories;
+  bool isViewSelected;
 
   DietSectionModel(
-      {required this.name, required this.iconPath, required this.amount});
-  List<DietSectionModel> getDietSection() {
+      {required this.name,
+      required this.iconPath,
+      required this.level,
+      required this.duration,
+      required this.calories,
+      required this.isViewSelected});
+
+  static List<DietSectionModel> getDietSection() {
     List<DietSectionModel> dietSection = [];
 
     dietSection.add(
       DietSectionModel(
-          name: "naflkjslgjme",
-          iconPath: "iconPakjkfjdkfjkdjth",
-          amount: 37654),
+          name: 'Honey',
+          iconPath: './assets/icon/honey-pancakes.svg',
+          level: 'Easy',
+          duration: '30 minutes',
+          calories: '200cal',
+          isViewSelected: false),
+    );
+
+    dietSection.add(
+      DietSectionModel(
+          name: 'Honey',
+          iconPath: './assets/icon/honey-pancakes.svg',
+          level: 'Hard',
+          duration: '30 minutes',
+          calories: '200cal',
+          isViewSelected: false),
+    );
+    dietSection.add(
+      DietSectionModel(
+          name: 'Honey',
+          iconPath: './assets/icon/honey-pancakes.svg',
+          level: 'Easy',
+          duration: '30 minutes',
+          calories: '200cal',
+          isViewSelected: false),
+    );
+    dietSection.add(
+      DietSectionModel(
+          name: 'Expert',
+          iconPath: './assets/icon/honey-pancakes.svg',
+          level: 'Easy',
+          duration: '30 minutes',
+          calories: '200cal',
+          isViewSelected: false),
     );
 
     return dietSection;
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter/widgets.dart';
 import 'package:flutter_01/model/category_model.dart';
+import 'package:flutter_01/model/deit_section_model.dart';
 import 'package:flutter_01/widgets/search_bar.dart';
-// import 'package:flutter_01/widgets/search_bar.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class Homepage extends StatefulWidget {
@@ -15,14 +14,16 @@ class Homepage extends StatefulWidget {
 
 class _HomepageState extends State<Homepage> {
   List<CategoryModel> categories = [];
+  List<DietSectionModel> dietSection = [];
 
-  void _getCategories() {
+  void _getInitialInfo() {
     categories = CategoryModel.getCategory();
+    dietSection = DietSectionModel.getDietSection();
   }
 
   @override
   Widget build(BuildContext context) {
-    _getCategories();
+    _getInitialInfo();
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -40,8 +41,9 @@ class _HomepageState extends State<Homepage> {
             alignment: Alignment.center,
             margin: const EdgeInsets.all(10.0),
             decoration: BoxDecoration(
-                color: Colors.black12,
-                borderRadius: BorderRadius.circular(8.0)),
+              color: Colors.black12,
+              borderRadius: BorderRadius.circular(8.0),
+            ),
             child: SvgPicture.asset(
               './assets/icon/arrowleft.svg',
             ),
@@ -72,10 +74,11 @@ class _HomepageState extends State<Homepage> {
         // crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const MySearchBar(),
-          //-------------------------- Category Section
           Column(
-            // crossAxisAlignment: CrossAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              //-------------------------- Category Section ===============
+
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -138,68 +141,9 @@ class _HomepageState extends State<Homepage> {
                   )
                 ],
               ),
-              //TODO: Modift the DIET SECTION and create a model data of the diet section.
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 30.0),
-                  const Padding(
-                    padding: EdgeInsets.only(left: 20),
-                    child: Text(
-                      "Diet Section",
-                      style: TextStyle(
-                          fontSize: 20.0, fontWeight: FontWeight.w800),
-                    ),
-                  ),
-                  const SizedBox(height: 15.0),
-                  SizedBox(
-                    height: 200.0,
-                    // color: Colors.green,
-                    child: ListView.separated(
-                      scrollDirection: Axis.horizontal,
-                      separatorBuilder: (context, index) => const SizedBox(
-                        width: 25.0,
-                      ),
-                      padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-                      itemCount: categories.length,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          width: 180,
-                          decoration: BoxDecoration(
-                            color: categories[index].boxColor,
-                            borderRadius: BorderRadius.circular(8.0),
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Container(
-                                height: 50,
-                                width: 50,
-                                decoration: const BoxDecoration(
-                                    color: Colors.white,
-                                    shape: BoxShape.circle),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child:
-                                      // SvgPicture.asset('./assets/icon/pie.svg'),
-                                      // TODO: always remeber to double or triple check the model data in order to render the SVG assets and avoid the white screen issue again.
-                                      SvgPicture.asset(
-                                          categories[index].iconPath),
-                                ),
-                              ),
-                              Text(
-                                categories[index].name,
-                                style: const TextStyle(
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.w600),
-                              )
-                            ],
-                          ),
-                        );
-                      },
-                    ),
-                  )
-                ],
+              // ------------------------------------Model Section ============================
+              const SizedBox(
+                height: 40.0,
               ),
             ],
           ),
